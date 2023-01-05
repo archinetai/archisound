115ae3516d2cfd495a339efb5523c7a63c745b42
Flavio Schneider <flavio.schneider.97@gmail.com>
2023-01-05 13:51:38 +0100

    feat: add dmae1d-ATC64-v2

--- archisound/archisound.py
+++ archisound/archisound.py
@@ -4,6 +4,7 @@ from transformers import AutoModel
 REVISION = {
     "autoencoder1d-AT-v1": "57b6cde1969208d10fdd3e813708c1abe49f25c1",
     "dmae1d-ATC64-v1": "07885065867977af43b460bb9c1422bdc90c29a0",
+    "dmae1d-ATC64-v2": "e6613882af746df3ed10add5a908aecd9e426178",
 }
 
 
