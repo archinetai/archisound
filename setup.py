42f2db5e08a81af9565cea85b8f6160662fff9dd
Flavio Schneider <flavio.schneider.97@gmail.com>
2023-01-13 16:12:25 +0100

    feat: update revision

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="archisound",
     packages=find_packages(exclude=[]),
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     description="ArchiSound",
     long_description_content_type="text/markdown",
