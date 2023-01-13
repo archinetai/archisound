e9d0c9f4555b10753f6230a8a61d20cbb86a1c72
Flavio Schneider <flavio.schneider.97@gmail.com>
2023-01-13 16:12:00 +0100

    feat: update revision

--- archisound/archisound.py
+++ archisound/archisound.py
@@ -4,7 +4,7 @@ from transformers import AutoModel
 REVISION = {
     "autoencoder1d-AT-v1": "57b6cde1969208d10fdd3e813708c1abe49f25c1",
     "dmae1d-ATC64-v1": "07885065867977af43b460bb9c1422bdc90c29a0",
-    "dmae1d-ATC64-v2": "e6613882af746df3ed10add5a908aecd9e426178",
+    "dmae1d-ATC64-v2": "3ffeea68d4c069777055fce9ac77bbb67eec1d68",
 }
 
 
