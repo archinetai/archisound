7d74ec2f4fac9487d5a95ba15125ae90939d5b09
Flavio Schneider <flavio.schneider.97@gmail.com>
2023-01-27 11:31:42 +0100

    feat: add adapter

--- archisound/archisound.py
+++ archisound/archisound.py
@@ -6,6 +6,7 @@ REVISION = {
     "dmae1d-ATC64-v1": "07885065867977af43b460bb9c1422bdc90c29a0",
     "dmae1d-ATC64-v2": "3ffeea68d4c069777055fce9ac77bbb67eec1d68",
     "dmae1d-ATC32-v3": "3d43b811b83fa395d5ccd6cf58b796b85fddd1d2",
+    "adapter-A-v1": "2ee66467450389917eab027526ea31c66d4b7edb",
 }
 
 
